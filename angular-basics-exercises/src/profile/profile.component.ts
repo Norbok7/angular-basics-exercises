aa12f590d909e0e98ed39198d2dea8f210657d14
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-09-27 23:53:33 -0500

    ex2

---
+++ angular-basics-exercises/src/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html'
+})
+export class ProfileComponent {
+
+name1 = 'garrett';
+age1 = 22;
+};
